9e051af6a8c38fe003b02be5fe271dc32fa422b1
Francisco Giordano <frangio.1@gmail.com>
2020-04-24 17:09:42 -0300

    fix types for getInheritanceChain

--- src/solc/get-inheritance-chain.ts
+++ src/solc/get-inheritance-chain.ts
@@ -1,9 +1,20 @@
 import { getContract } from './ast-utils';
 import { Artifact } from './artifact';
 
-export function getInheritanceChain(contract: string, contractsToArtifactsMap: Record<string, Artifact>): Artifact[] {
+export function getInheritanceChain(contract: string, contractsToArtifactsMap: Record<string | number, Artifact | undefined>): Artifact[] {
   const art = contractsToArtifactsMap[contract];
+
+  if (art === undefined) {
+    throw new Error(`Artifact for ${contract} is not available`);
+  }
+
   const contractNode = getContract(art);
 
-  return contractNode.linearizedBaseContracts.map(base => contractsToArtifactsMap[base]);
+  return contractNode.linearizedBaseContracts.map(base => {
+    const baseArt = contractsToArtifactsMap[base];
+    if (baseArt === undefined) {
+      throw new Error(`One of the base artifacts for ${contract} is not available`);
+    }
+    return baseArt;
+  });
 }
