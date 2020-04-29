aa0c3b23be29896ec11cd52b319d576c55a85689
Francisco Giordano <frangio.1@gmail.com>
2020-04-28 21:06:26 -0300

    fix test for new path normalization

--- test/solc-0.5/test/LocalContract.behaviour.ts
+++ test/solc-0.5/test/LocalContract.behaviour.ts
@@ -9,7 +9,7 @@ export function shouldTranspileToValidContract(contract: string, output: Record<
       const key = file.contracts[0];
       expect(file.source).toMatchSnapshot();
       expect(file.fileName).toBe(`${output[key].fileName}.sol`);
-      expect(file.path).toBe(`./contracts/__upgradeable__/${output[key].path}Upgradeable.sol`);
+      expect(file.path).toBe(`contracts/__upgradeable__/${output[key].path}Upgradeable.sol`);
       expect(file.contracts).toEqual(output[key].contracts);
     }
   });
