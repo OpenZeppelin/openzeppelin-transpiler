2258f6e1878e30d5496ce6b8f7ecd30cb1ffec83
Francisco Giordano <frangio.1@gmail.com>
2020-05-05 18:18:15 -0300

    convert fix-import-directives to generator function

--- src/transformations/fix-import-directives.ts
+++ src/transformations/fix-import-directives.ts
@@ -4,59 +4,57 @@ import { getImportDirectives, getSourceIndices } from '../solc/ast-utils';
 import { Artifact } from '../solc/artifact';
 import { Transformation } from '../transformation';
 
-export function fixImportDirectives(
+export function* fixImportDirectives(
   artifact: Artifact,
   artifacts: Artifact[],
   contracts: Artifact[],
-): Transformation[] {
+): Generator<Transformation> {
   const dirname = path.dirname(artifact.sourcePath);
 
   const imports = getImportDirectives(artifact.ast);
 
-  return imports
-    .map(imp => {
-      const transformed = [];
+  for (const imp of imports) {
+    const transformed = [];
 
-      const isLocal = artifacts.some(
-        art => art.ast.id === imp.sourceUnit && art.sourcePath.startsWith('.')
+    const isLocal = artifacts.some(
+      art => art.ast.id === imp.sourceUnit && art.sourcePath.startsWith('.')
+    );
+
+    // imports the original file
+    if (isLocal) {
+      transformed.push(
+        path.relative(
+          path.join('__upgradeable__', dirname),
+          path.join(dirname, imp.file),
+        )
       );
+    } else {
+      transformed.push(imp.file);
+    }
 
-      // imports the original file
+    const isTranspiled = artifacts.some(
+      art => art.ast.id === imp.sourceUnit && contracts.includes(art)
+    );
+
+    // imports the transpiled file
+    if (isTranspiled) {
       if (isLocal) {
-        transformed.push(
-          path.relative(
-            path.join('__upgradeable__', dirname),
-            path.join(dirname, imp.file),
-          )
-        );
+        transformed.unshift(imp.file); // TODO: may not be a relative path
       } else {
-        transformed.push(imp.file);
+        transformed.unshift(path.relative(dirname, imp.file));
       }
+    }
 
-      const isTranspiled = artifacts.some(
-        art => art.ast.id === imp.sourceUnit && contracts.includes(art)
-      );
+    const finalTransformation = transformed.map(t => `import "${t}";`).join('\n');
+    const [start, len] = getSourceIndices(imp);
 
-      // imports the transpiled file
-      if (isTranspiled) {
-        if (isLocal) {
-          transformed.unshift(imp.file); // TODO: may not be a relative path
-        } else {
-          transformed.unshift(path.relative(dirname, imp.file));
-        }
-      }
-
-      const finalTransformation = transformed.map(t => `import "${t}";`).join('\n');
-      const [start, len] = getSourceIndices(imp);
-
-      return !isLocal && !isTranspiled
-        ? null
-        : {
-            kind: 'fix-import-directives',
-            start: start,
-            end: start + len,
-            text: finalTransformation,
-          } as Transformation;
-    })
-    .filter((transformation): transformation is Transformation => transformation !== null);
+    if (isLocal || isTranspiled) {
+      yield {
+        kind: 'fix-import-directives',
+        start: start,
+        end: start + len,
+        text: finalTransformation,
+      };
+    }
+  }
 }
