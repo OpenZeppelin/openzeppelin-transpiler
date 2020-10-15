232375c2712d9db9ccb75772b6642b03f6858170
Francisco Giordano <frangio.1@gmail.com>
2020-10-14 21:26:58 -0300

    fix test script

--- test/solc-0.6/test.sh
+++ test/solc-0.6/test.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 cd "$(dirname "$BASH_SOURCE")"
 
-rm -rf build/contracts contracts/__upgradable__
+rm -rf build/contracts contracts/__upgradeable__
 oz compile --no-interactive
 jest --verbose
