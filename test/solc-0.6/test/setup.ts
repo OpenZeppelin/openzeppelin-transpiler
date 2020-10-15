659f8d06f39283a739e8dd3cb1f794ce9e8b9492
Francisco Giordano <frangio.1@gmail.com>
2020-10-14 21:28:46 -0300

    remove compile test

--- test/solc-0.6/test/setup.ts
+++ test/solc-0.6/test/setup.ts
@@ -1,28 +1,4 @@
 import fs from 'fs-extra';
-import path from 'path';
-import util from 'util';
-import child from 'child_process';
-
-const exec = util.promisify(child.exec);
-
-import { transpileContracts } from '../../../src/';
-
-export async function transpileAndSaveContracts(contracts: string[], dir: string): Promise<void> {
-  const artifacts = fs.readdirSync(dir).map(file => {
-    return JSON.parse(fs.readFileSync(`${dir}${file}`).toString());
-  });
-
-  const output = transpileContracts(contracts, artifacts, './contracts');
-
-  for (const file of output) {
-    await fs.ensureDir(path.dirname(file.path));
-    fs.writeFileSync(file.path, file.source);
-  }
-}
-
-export async function compileContracts(): Promise<void> {
-  await exec('oz compile --no-interactive');
-}
 
 export const artifacts = fs.readdirSync('./build/contracts/').map(file => {
   return JSON.parse(fs.readFileSync(`./build/contracts/${file}`).toString());
