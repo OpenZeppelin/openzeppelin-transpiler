638eda27bb9f1ce0aeabb1957268bf86db4d2193
Francisco Giordano <frangio.1@gmail.com>
2020-10-29 16:21:17 -0300

    use 4 spaces for solidity intentation

--- test/solc-0.6/contracts/Rename.sol
+++ test/solc-0.6/contracts/Rename.sol
@@ -1,25 +1,25 @@
 pragma solidity ^0.6.0;
 
 library RenameLibrary {
-  function test() external {
-  }
+    function test() external {
+    }
 }
 
 contract RenameContract {
 }
 
 contract RenameDeployer {
-  RenameContract rc = RenameContract(0);
+    RenameContract rc = RenameContract(0);
 
-  constructor() public {
-    new RenameContract();
-  }
+    constructor() public {
+        new RenameContract();
+    }
 
-  function deploy() external returns (RenameContract) {
-    return new RenameContract();
-  }
+    function deploy() external returns (RenameContract) {
+        return new RenameContract();
+    }
 
-  function test() external {
-    RenameLibrary.test();
-  }
+    function test() external {
+        RenameLibrary.test();
+    }
 }
