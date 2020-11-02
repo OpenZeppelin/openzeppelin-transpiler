ceed123ccaca6c62bf23ff6038ff03a5e0d799a2
Francisco Giordano <frangio.1@gmail.com>
2020-11-02 20:07:10 -0300

    fix import directive transformation when aliased

--- src/transformations/fix-import-directives.ts
+++ src/transformations/fix-import-directives.ts
@@ -2,13 +2,45 @@ import { SourceUnit } from 'solidity-ast';
 import { findAll } from 'solidity-ast/utils';
 import { getNodeBounds } from '../solc/ast-utils';
 import { Transformation } from './type';
-import { renamePath } from '../rename';
+import { renameContract, renamePath } from '../rename';
+import { ASTResolver } from '../ast-resolver';
 
-export function* fixImportDirectives(ast: SourceUnit): Generator<Transformation> {
+export function* fixImportDirectives(
+  ast: SourceUnit,
+  resolver: ASTResolver,
+): Generator<Transformation> {
   for (const imp of findAll('ImportDirective', ast)) {
+    const referencedSourceUnit = resolver.resolveNode('SourceUnit', imp.sourceUnit);
+    if (referencedSourceUnit === undefined) {
+      throw new Error(`Can't find SourceUnit node for ${imp.file}`);
+    }
+
+    const aliases = imp.symbolAliases.map(a => {
+      const id = referencedSourceUnit.exportedSymbols[a.foreign.name]?.[0];
+      if (id === undefined) {
+        throw new Error(`Can't find symbol imported in ${ast.absolutePath}`);
+      }
+      let alias = '';
+      if (resolver.resolveContract(id) === undefined) {
+        alias += a.foreign.name;
+      } else {
+        alias += renameContract(a.foreign.name);
+      }
+      if (a.local != null && a.local !== a.foreign.name) {
+        alias += ` as ${a.local}`;
+      }
+      return alias;
+    });
+
+    const statement = ['import'];
+    if (aliases.length > 0) {
+      statement.push(`{ ${aliases.join(', ')} } from`);
+    }
+    statement.push(`"${renamePath(imp.file)}";`);
+
     yield {
       kind: 'fix-import-directives',
-      text: `import "${renamePath(imp.file)}";`,
+      text: statement.join(' '),
       ...getNodeBounds(imp),
     };
   }
