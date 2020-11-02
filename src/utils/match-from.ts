b565ec2737d1032dc97c50b261aa7e99ae28475d
Francisco Giordano <frangio.1@gmail.com>
2020-11-02 16:15:54 -0300

    fix match-from

--- src/utils/match-from.ts
+++ src/utils/match-from.ts
@@ -1,5 +1,5 @@
 export function matchFrom(str: string, re: RegExp, index: number): RegExpExecArray | null {
-  const re2 = new RegExp(re, 'y');
+  const re2 = new RegExp(re, 'g');
   re2.lastIndex = index;
   return re2.exec(str);
 }
