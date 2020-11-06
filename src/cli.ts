d4aefdbe78aaa6a37f1b901b4d5bb0028c094ba4
Francisco Giordano <frangio.1@gmail.com>
2020-11-06 14:54:34 -0300

    add -D option to delete original files

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
+import 'source-map-support/register';
 import { promises as fs } from 'fs';
 import path from 'path';
-import 'source-map-support/register';
+import minimist from 'minimist';
 
 import bre from '@nomiclabs/buidler';
 
@@ -10,6 +11,8 @@ import { transpile } from '.';
 import { SolcOutput, SolcInput } from './solc/input-output';
 
 async function main() {
+  const { D: deleteOriginals } = minimist(process.argv.slice(2));
+
   const solcInputPath = path.join(bre.config.paths.cache, 'solc-input.json');
   const solcInput: SolcInput = JSON.parse(await fs.readFile(solcInputPath, 'utf8'));
   const solcOutputPath = path.join(bre.config.paths.cache, 'solc-output.json');
@@ -18,10 +21,20 @@ async function main() {
 
   await Promise.all(
     transpiled.map(async t => {
-      await fs.mkdir(path.dirname(t.path), { recursive: true });
-      await fs.writeFile(t.path, t.source);
+      const outputPath = path.join(bre.config.paths.root, t.path);
+      await fs.mkdir(path.dirname(outputPath), { recursive: true });
+      await fs.writeFile(outputPath, t.source);
     }),
   );
+
+  if (deleteOriginals) {
+    const generated = new Set(transpiled.map(t => path.join(bre.config.paths.root, t.path)));
+    const originals = Object.keys(solcOutput.sources)
+      .map(s => path.join(bre.config.paths.root, s))
+      .filter(p => !generated.has(p));
+
+    await Promise.all(originals.map(p => fs.unlink(p)));
+  }
 }
 
 main().catch(e => {
