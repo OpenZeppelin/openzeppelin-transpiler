0dfe4a745f5af72171ad86808dd2d09932f42959
Francisco Giordano <frangio.1@gmail.com>
2021-06-24 23:35:11 -0300

    remove other unnecessary regex

--- src/transformations/utils/build-pulic-initialize.ts
+++ src/transformations/utils/build-pulic-initialize.ts
@@ -12,18 +12,12 @@ export function buildPublicInitialize(
   let args = '';
   let argNames = '';
   if (ctor) {
-    const ctorSource = tools.readOriginal(ctor);
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const argsMatch = ctorSource.match(/\((.*?)\)/s);
-    if (argsMatch === null) {
-      throw new Error(`Could not find constructor arguments for ${contract.name}`);
-    }
-    args = argsMatch[1];
+    args = tools.readOriginal(ctor.parameters);
     argNames = ctor.parameters.parameters.map(p => p.name).join(', ');
   }
 
   return formatLines(1, [
-    `function initialize(${args}) public virtual initializer {`,
+    `function initialize${args} public virtual initializer {`,
     [`__${contract.name}_init(${argNames});`],
     '}',
   ]);
