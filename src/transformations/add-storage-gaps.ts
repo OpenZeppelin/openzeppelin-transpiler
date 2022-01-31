d728ce8750d6c02e278461713765a763ab0386b7
JulissaDantes <julissa@singularsound.com>
2022-01-31 15:35:57 -0400

    Add gap variable docstring (#73)
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- src/transformations/add-storage-gaps.ts
+++ src/transformations/add-storage-gaps.ts
@@ -21,7 +21,17 @@ export function* addStorageGaps(
       const contractBounds = getNodeBounds(contract);
       const start = contractBounds.start + contractBounds.length - 1;
 
-      const text = formatLines(1, [`uint256[${gapSize}] private __gap;`]);
+      const text = formatLines(0, [
+        ``,
+        [
+          `/**`,
+          ` * This empty reserved space is put in place to allow future versions to add new`,
+          ` * variables without shifting down storage in the inheritance chain.`,
+          ` * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps`,
+          ` */`,
+          `uint256[${gapSize}] private __gap;`,
+        ],
+      ]);
 
       yield {
         kind: 'add-storage-gaps',
