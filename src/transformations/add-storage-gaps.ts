e2fa986cad6f03349974dea07a0eecb3b74cab2c
Francisco Giordano <frangio.1@gmail.com>
2022-01-31 16:33:34 -0300

    refactor use of formatLines

--- src/transformations/add-storage-gaps.ts
+++ src/transformations/add-storage-gaps.ts
@@ -22,13 +22,15 @@ export function* addStorageGaps(
       const start = contractBounds.start + contractBounds.length - 1;
 
       const text = formatLines(0, [
-        [``],
-        [`/**`],
-        [`* This empty reserved space is put in place to allow future versions to add new`],
-        [`* variables without shifting down storage in the inheritance chain.`],
-        [`* See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps`],
-        [`*/`],
-        [`uint256[${gapSize}] private __gap;`],
+        ``,
+        [
+          `/**`,
+          `* This empty reserved space is put in place to allow future versions to add new`,
+          `* variables without shifting down storage in the inheritance chain.`,
+          `* See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps`,
+          `*/`,
+          `uint256[${gapSize}] private __gap;`,
+        ],
       ]);
 
       yield {
