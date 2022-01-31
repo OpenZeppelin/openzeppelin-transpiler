1f32cddf6f6762b9de0644248f8775d129c06cd3
Francisco Giordano <frangio.1@gmail.com>
2022-01-31 16:34:01 -0300

    fix docstring indentation

--- src/transformations/add-storage-gaps.ts
+++ src/transformations/add-storage-gaps.ts
@@ -25,10 +25,10 @@ export function* addStorageGaps(
         ``,
         [
           `/**`,
-          `* This empty reserved space is put in place to allow future versions to add new`,
-          `* variables without shifting down storage in the inheritance chain.`,
-          `* See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps`,
-          `*/`,
+          ` * This empty reserved space is put in place to allow future versions to add new`,
+          ` * variables without shifting down storage in the inheritance chain.`,
+          ` * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps`,
+          ` */`,
           `uint256[${gapSize}] private __gap;`,
         ],
       ]);
