82ac72c87d1ebbe7ed19b6c6913444265dab331c
Francisco Giordano <frangio.1@gmail.com>
2022-02-04 18:00:03 -0300

    rename invalid test contracts

--- src/transform.test.ts
+++ src/transform.test.ts
@@ -149,13 +149,13 @@ test('invalid constructors', t => {
   // InvalidTransformConstructor
   const localContext = t.context;
   t.context.transform = new Transform(localContext.solcInput, localContext.solcOutput, {
-    exclude: source => source !== 'contracts/invalid/InvalidTransformConstructor.sol',
+    exclude: source => source !== 'contracts/invalid/TransformConstructorVarSubexpr.sol',
   });
 
   t.throws(() => t.context.transform.apply(transformConstructor));
   // InvalidTransformConstructorFunction
   t.context.transform = new Transform(localContext.solcInput, localContext.solcOutput, {
-    exclude: source => source !== 'contracts/invalid/InvalidTransformConstructorFunction.sol',
+    exclude: source => source !== 'contracts/invalid/TransformConstructorDupExpr.sol',
   });
   t.throws(() => t.context.transform.apply(transformConstructor));
 });
