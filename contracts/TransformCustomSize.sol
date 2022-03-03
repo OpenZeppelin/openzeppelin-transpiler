28dfb5b960f7d44eaf31e0c7d3deb7ea52d64b12
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-03-03 17:20:09 +0100

    improve test contracts

--- contracts/TransformCustomSize.sol
+++ contracts/TransformCustomSize.sol
@@ -7,38 +7,46 @@ struct HalfASlot {
 }
 
 contract SizeDefault {
-    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable state-variable-assignment
-    uint immutable w = block.number;
-    uint immutable x;
-    uint constant y = 1;
-    uint224 z0;
-    uint256 z1;
-    uint32  z2;
-    HalfASlot s1;
-    HalfASlot s2;
-    uint64[5] a1;
-    uint64[3] a2;
+  /// @custom:oz-upgrades-unsafe-allow state-variable-immutable state-variable-assignment
+  uint immutable w = block.number;
+  uint immutable x; // slot 0 (after conversion to private)
+  uint constant y = 1;
+  uint224 z0; // slot 1
+  uint256 z1; // slot 2
+  uint32 z2; // slot 3
+  HalfASlot s1; // slot 4+5
+  HalfASlot s2; // slot 6+7
+  uint32 z3; // slot 8
+  uint32 z4; // slot 8
+  uint32 z5; // slot 8
+  uint64[5] a1; // slot 9+10
+  uint64[3] a2; // slot 11
 
-    constructor(uint _x) {
-      x = _x;
-    }
+  constructor(uint _x) {
+    x = _x;
+  }
+  // gap should be 38 = 50 - 12
 }
 
 /// @custom:contract-size 128
 contract SizeOverride {
-    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable state-variable-assignment
-    uint immutable w = block.number;
-    uint immutable x;
-    uint constant y = 1;
-    uint224 z0;
-    uint256 z1;
-    uint32  z2;
-    HalfASlot s1;
-    HalfASlot s2;
-    uint64[5] a1;
-    uint64[3] a2;
+  /// @custom:oz-upgrades-unsafe-allow state-variable-immutable state-variable-assignment
+  uint immutable w = block.number;
+  uint immutable x; // slot 0 (after conversion to private)
+  uint constant y = 1;
+  uint224 z0; // slot 1
+  uint256 z1; // slot 2
+  uint32 z2; // slot 3
+  HalfASlot s1; // slot 4+5
+  HalfASlot s2; // slot 6+7
+  uint32 z3; // slot 8
+  uint32 z4; // slot 8
+  uint32 z5; // slot 8
+  uint64[5] a1; // slot 9+10
+  uint64[3] a2; // slot 11
 
-    constructor(uint _x) {
-      x = _x;
-    }
+  constructor(uint _x) {
+    x = _x;
+  }
+  // gap should be 116 = 128 - 12
 }
