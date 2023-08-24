9865f0578c0f9b1cd5a9a3f2571ddd5fc1a6dd17
Francisco Giordano <fg@frang.io>
2023-08-24 12:01:54 -0300

    disable compilation warnings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 const { internalTask } = require('hardhat/config');
 const { TASK_COMPILE_SOLIDITY_GET_COMPILER_INPUT } = require('hardhat/builtin-tasks/task-names');
 
+require('hardhat-ignore-warnings');
+
 internalTask(TASK_COMPILE_SOLIDITY_GET_COMPILER_INPUT, async (args, hre, runSuper) => {
   const input = await runSuper();
   input.settings.outputSelection['*']['*'].push('storageLayout');
@@ -11,4 +13,5 @@ module.exports = {
   solidity: {
     compilers: ['0.6.7', '0.8.8', '0.8.20'].map(version => ({ version })),
   },
+  warnings: 'off',
 };
