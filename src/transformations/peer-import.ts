d4a70dd0a346f8bde0ab8a500c8943fc8458928b
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-09-25 16:05:12 +0200

    Update src/transformations/peer-import.ts
    Co-authored-by: Francisco <fg@frang.io>

--- src/transformations/peer-import.ts
+++ src/transformations/peer-import.ts
@@ -8,29 +8,13 @@ export function* peerImport(
   { getData }: TransformerTools,
 ): Generator<Transformation> {
   for (const node of ast.nodes) {
-    switch (node.nodeType) {
-      case 'ContractDefinition':
-      case 'EnumDefinition':
-      case 'ErrorDefinition':
-      case 'FunctionDefinition':
-      case 'StructDefinition':
-      case 'UserDefinedValueTypeDefinition':
-      case 'VariableDeclaration': {
-        const { importFromPeer } = getData(node);
-        if (importFromPeer !== undefined) {
-          yield {
-            ...getNodeBounds(node),
-            kind: 'replace-declaration-with-peer-import',
-            text: `import { ${node.name} } from "${importFromPeer}";`,
-          };
-        }
-        break;
-      }
-      case 'ImportDirective':
-      case 'PragmaDirective':
-      case 'UsingForDirective': {
-        break;
-      }
+    const { importFromPeer } = getData(node);
+    if (importFromPeer !== undefined) {
+      yield {
+        ...getNodeBounds(node),
+        kind: 'replace-declaration-with-peer-import',
+        text: `import { ${node.name} } from "${importFromPeer}";`,
+      };
     }
   }
 }
