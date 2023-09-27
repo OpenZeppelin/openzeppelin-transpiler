1caf2eb0e836e2597789a8068e0571cc15533eed
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-09-27 16:08:32 +0200

    Fix rename-inheritdoc with peer project (#133)

--- src/transformations/rename-inheritdoc.ts
+++ src/transformations/rename-inheritdoc.ts
@@ -9,20 +9,29 @@ import { matchBuffer } from '../utils/match';
 
 export function* renameInheritdoc(
   sourceUnit: SourceUnit,
-  { readOriginal }: TransformerTools,
+  { readOriginal, getData, resolver }: TransformerTools,
 ): Generator<Transformation> {
-  for (const doc of findAll('StructuredDocumentation', sourceUnit)) {
-    const bounds = getNodeBounds(doc);
-    const re = /(@inheritdoc\s+)([a-zA-Z0-9$_]+)/;
-    const match = matchBuffer(readOriginal(doc, 'buffer'), re);
+  for (const contract of findAll('ContractDefinition', sourceUnit)) {
+    const baseContracts = contract.linearizedBaseContracts.map(i => resolver.resolveContract(i));
 
-    if (match) {
-      yield {
-        start: bounds.start + match.start + match.captureLengths[0],
-        length: match.captureLengths[1],
-        kind: 'rename-inheritdoc',
-        transform: source => source.replace(/[a-zA-Z0-9$_]+$/, renameContract),
-      };
+    for (const doc of findAll('StructuredDocumentation', contract)) {
+      const bounds = getNodeBounds(doc);
+      const re = /(@inheritdoc\s+)([a-zA-Z0-9$_]+)/;
+      const match = matchBuffer(readOriginal(doc, 'buffer'), re);
+
+      if (match) {
+        yield {
+          start: bounds.start + match.start + match.captureLengths[0],
+          length: match.captureLengths[1],
+          kind: 'rename-inheritdoc',
+          transform: source => {
+            const ref = baseContracts.find(c => c?.canonicalName == source);
+            return ref !== undefined && getData(ref).importFromPeer !== undefined
+              ? source
+              : source.replace(/[a-zA-Z0-9$_]+$/, renameContract);
+          },
+        };
+      }
     }
   }
 }
