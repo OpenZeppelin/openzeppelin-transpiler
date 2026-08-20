aec4ec52defb05cd3386a088d0bf5e2af6ff7c46
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-08-20 13:51:42 +0200

    Fix prettier formatting

--- src/transformations/transform-optional-initializer.ts
+++ src/transformations/transform-optional-initializer.ts
@@ -14,7 +14,9 @@ export function* transformOptionalInitializer(
     for (const fnDecl of findAll('FunctionDefinition', contract)) {
       if (extractOptionalInitializer(fnDecl)) {
         if (fnDecl.visibility !== 'internal') {
-          throw new Error(`@custom:oz-upgrades-optional-initializer can only be used on internal functions, but found on ${fnDecl.name} in contract ${contract.name}`);
+          throw new Error(
+            `@custom:oz-upgrades-optional-initializer can only be used on internal functions, but found on ${fnDecl.name} in contract ${contract.name}`,
+          );
         }
         if (fnDecl.modifiers.some(mod => mod.modifierName.name === 'onlyInitializing')) {
           continue;
